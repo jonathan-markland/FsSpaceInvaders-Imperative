33b725ca04f681f410671e50868d74e2408e4e81
Jonathan Markland <jonathan.markland@btinternet.com>
2019-12-25 15:49:45 +0000

    Tidy

--- FsSpaceInvaders/Rates.fs
+++ FsSpaceInvaders/Rates.fs
@@ -2,10 +2,10 @@
 
 open Mechanics
 
-let TimeForMothershipCheck = TickSpan(5000u)
-let TimeForMothershipUpdate = TickSpan(10u)
-let TimeForNewBombCheck = TickSpan(500u)  // TODO: Should this be run-time computed.  Change this each level?
-let TimeForBombUpdateCheck = TickSpan(5u)
-let TimeForReloadShipWeapon = TickSpan(50u)
-let TimeForWholeExplosion = TickSpan(200u)
-let TimeForEndState = TickSpan(2500u)
+let TimeForMothershipCheck   = TickSpan(5000u)
+let TimeForMothershipUpdate  = TickSpan(10u)
+let TimeForNewBombCheck      = TickSpan(500u)  // TODO: Should this be run-time computed.  Change this each level?
+let TimeForBombUpdateCheck   = TickSpan(5u)
+let TimeForReloadShipWeapon  = TickSpan(50u)
+let TimeForWholeExplosion    = TickSpan(200u)
+let TimeForEndState          = TickSpan(2500u)
